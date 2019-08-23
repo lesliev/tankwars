c82395f82c18f0d365149ed58c259c35a4939ceb
Leslie Viljoen <leslieviljoen@gmail.com>
2019-08-23 12:44:24 +1200

    Add apples!!!

--- tanks.js
+++ tanks.js
@@ -5,6 +5,7 @@ var state = 'menu';
 var canvasWidth = 1802;
 var canvasHeight = 900;
 var background = new Image();
+var bulletImage = new Image();
 var crashSound = new Audio('crash.mp3');
 
 var tankTypes = [
@@ -22,7 +23,14 @@ var tanks = [
     x: canvasWidth/4,
     y: canvasHeight - canvasHeight/4,
     r: 0,
-    dotStyle: "#00ffff"
+    dotStyle: "#00ffff",
+    keys: {
+      left: 65,
+      right: 68,
+      forward: 87,
+      backward: 83,
+      fire: 32
+    }
   },
   {
     type: tankTypes[2],
@@ -30,9 +38,20 @@ var tanks = [
     x: canvasWidth - canvasWidth/4,
     y: canvasHeight - canvasHeight/4,
     r: 0,
-    dotStyle: "#ff0000"
+    dotStyle: "#ff0000",
+    keys: {
+      left: 37,
+      right: 39,
+      forward: 38,
+      backward: 40,
+      fire: 80
+    }
   }
 ]
+
+var bullets = [];
+
+var tankNum = tanks.length;
 
 
 window.onkeyup = function(e) { 
@@ -47,11 +66,12 @@ window.onkeydown = function(e) {
 
 function init() {
 
-  for(i = 0; i < 2; i+=1) {
+  for(i = 0; i < tankNum; i+=1) {
     tanks[i].image.src = tanks[i].type.fileName;
   }
 
   background.src = 'infinigrass.jpg';
+  bulletImage.src = 'apple_small.png'
 
   window.requestAnimationFrame(draw);
 }
@@ -109,42 +129,57 @@ function draw() {
       crashSound.play();
     }
 
-    console.log(distance);
+    for(i = 0; i < tankNum; i += 1) {
+      if((keys[tanks[i].keys.left] == true)) { tanks[i].r -= 0.1; }
+      if((keys[tanks[i].keys.right] == true)) { tanks[i].r += 0.1; }
+      if(keys[tanks[i].keys.forward] == true) {
+        tanks[i].x += 2 * Math.sin(tanks[i].r);
+        tanks[i].y -= 2 * Math.cos(tanks[i].r);
+      }
+      if(keys[tanks[i].keys.backward] == true) {
+        tanks[i].x -= 2 * Math.sin(tanks[i].r);
+        tanks[i].y += 2 * Math.cos(tanks[i].r);
+      }
+      if(keys[tanks[i].keys.fire] == true) {
+        bullets.push( 
+          {
+            x: tanks[i].x,
+            y: tanks[i].y, 
+            r: tanks[i].r, 
+            image: bulletImage
+          }
+        );
+      }
+    }
 
-    if((keys[37] == true)) { tanks[1].r -= 0.1; }
-    if((keys[39] == true)) { tanks[1].r += 0.1; }
-    if(keys[38] == true) {
-      tanks[1].x += 2 * Math.sin(tanks[1].r);
-      tanks[1].y -= 2 * Math.cos(tanks[1].r);
-    }
-    if(keys[40] == true) {
-      tanks[1].x -= 2 * Math.sin(tanks[1].r);
-      tanks[1].y += 2 * Math.cos(tanks[1].r);
-    }
+    for(i = 0; i < tankNum; i += 1) {
+      if(tanks[i].x < 0){ tanks[i].x = 0; }
+      if(tanks[i].x > canvasWidth){ tanks[i].x = canvasWidth; }
 
-    if((keys[65] == true)) { tanks[0].r -= 0.1; }
-    if((keys[68] == true)) { tanks[0].r += 0.1; }
-    if(keys[87] == true) {
-      tanks[0].x += 2 * Math.sin(tanks[0].r);
-      tanks[0].y -= 2 * Math.cos(tanks[0].r);
+      if(tanks[i].y < 0){ tanks[i].y = 0; }
+      if(tanks[i].y > canvasHeight){ tanks[i].y = canvasHeight; }
     }
-    if(keys[83] == true) {
-      tanks[0].x -= 2 * Math.sin(tanks[0].r);
-      tanks[0].y += 2 * Math.cos(tanks[0].r);
-    }
-
-    for(tankNumber = 0; tankNumber < 2; tankNumber++) {
+    
+    // paint tanks
+    for(tankNumber = 0; tankNumber < tankNum; tankNumber++) {
       rotateAndPaintImage(ctx, tanks[tankNumber].image, tanks[tankNumber].r, 
         tanks[tankNumber].x, tanks[tankNumber].y, 
         17, 36.5);
 
-        ctx.beginPath();
-        ctx.rect(tanks[tankNumber].x, tanks[tankNumber].y, 8, 8);
-        
-        ctx.fillStyle = tanks[tankNumber].dotStyle;
+      // ctx.beginPath();
+      // ctx.rect(tanks[tankNumber].x, tanks[tankNumber].y, 8, 8);
+      
+      // ctx.fillStyle = tanks[tankNumber].dotStyle;
 
-        ctx.fill();
-        ctx.closePath();
+      // ctx.fill();
+      // ctx.closePath();
+    }
+
+    // paint bullets
+    for(bulletNumber = 0; bulletNumber < bullets.length; bulletNumber++) {
+      rotateAndPaintImage(ctx, bullets[bulletNumber].image, bullets[bulletNumber].r, 
+        bullets[bulletNumber].x, bullets[bulletNumber].y, 
+        8, 9);            
     }
 
     // 
